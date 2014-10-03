ea08d948334cd41a09c897be9540720c9ed72c58
MaurinElectroTextile <maurin.box@gmail.com>
2014-10-03 21:08:24 +0200

    complete rewrite of the project

---
+++ Teensy3.1_matrix/Matrix_16_16/Matrix_16_16.ino
@@ -0,0 +1,40 @@
+/////////////////////////////// MATRIX
+#define ROW            16
+#define COLUMN         16
+#define BAUDRATE       115200
+
+// Dig pins
+const int rowPins[ROW] = {
+  0, 1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+};
+
+// Analog pins
+const int columnPins[COLUMN] = {
+  A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15
+};
+
+void setup() {
+  Serial.begin(BAUDRATE);
+  // Set row pin in high-impedance state
+  for (int i=0; i<ROW; i++) {
+    pinMode(rowPins[ROW], INPUT);
+  }
+}
+
+/////////////////////// the loop
+void loop() {
+  for (int row=0; row<ROW; row++) {
+    // Set row pin as output +3V
+    pinMode(rowPins[row], OUTPUT);
+    digitalWrite(rowPins[row], HIGH);
+    
+    for ( int column=0; column<COLUMN; column++ ) {
+      int value = analogRead( columnPins[column] );
+      Serial.write( value & B01111111 );          // lowByte
+      Serial.write( (value >> 7) & B00000111 );   // highByte
+    }
+    // Set row pin in high-impedance state
+    pinMode(rowPins[row], INPUT);
+  }
+  Serial.write( 255 ); // footer
+}
