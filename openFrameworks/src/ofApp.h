e668f3189003fcacc5805657b3b4c9dc49ca82b3
maurinElectroTextile <maurin@datapaulette.org>
2017-01-03 16:08:09 +0100

    Clean up

--- openFrameworks/src/ofApp.h
+++ openFrameworks/src/ofApp.h
@@ -9,17 +9,16 @@
 
 #define USB_PORT        "/dev/ttyACM0"
 #define BAUD_RATE       230400  // With Teensy, it's always the same native speed. The baud rate setting is ignored.
-#define DATAS		    512     // numbur of bytes received from teensy
+#define DATAS           256     // numbur of bytes received from the teensy
 #define ROWS            16
-#define COLS 	        16
+#define COLS            16
 #define X_NEWSIZE       64
 #define Y_NEWSIZE       64
 
-#define HOST            "localhost"
-//#define HOST 		"10.42.0.255"
-#define UDP_OUTPUT_PORT 7771
-#define UDP_INPUT_PORT  1234
-#define SYNTH           8
+#define HOST             "localhost"
+//#define HOST           "10.42.0.255"
+#define UDP_OUTPUT_PORT  7771
+#define UDP_INPUT_PORT   1234
 
 struct centroid
 {
@@ -49,10 +48,10 @@ public:
     int16_t                 highBit;
     int16_t                 lowBit;
     int                     sensorID;
-    uint8_t                 serialData[ DATAS ];   		    //
-    uint8_t                 storedValueRast[ ROWS * COLS ];	// 1D array
+    uint8_t                 serialData[DATAS];             //
+    uint8_t                 storedValueRast[ROWS * COLS];  // 1D array
     int8_t                  threshold;
-    bool 		            newFrame;
+    bool                    newFrame;
     ofPixels                interpolatedFrame;
     ofxCvGrayscaleImage     grayImage;
     ofxCvGrayscaleImage     grayBg;
@@ -61,7 +60,6 @@ public:
     bool                    bLearnBakground;
     ofxCvBlob               blob;
     ofPixels                grayImageCopy;
-    //uint8_t               higerValuePos;
     void                    toggleDspPressed(bool & toggleState);
     void                    sliderVolumeValue(float & sliderValue_A);
     void                    sliderTranspositionValue(float & sliderValue_B);
@@ -76,7 +74,6 @@ public:
     ofMesh                  mesh;
     ofxOscSender            sender;
     ofxOscReceiver          receiver;
-    ofTrueTypeFont	        font;
+    ofTrueTypeFont          font;
     vector<centroid>        centroids;
 };
-
