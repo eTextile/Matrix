10980f6d467c67e852ca1f9d6d9c49d8e1d20994
maurinElectroTextile <maurin@datapaulette.org>
2017-08-24 10:49:24 +0200

    Updated Sketch

--- Arduino/bilinear_interpolation/bilinear_interpolation.ino
+++ Arduino/bilinear_interpolation/bilinear_interpolation.ino
@@ -1,3 +1,4 @@
+// https://github.com/ARM-software/CMSIS/blob/master/CMSIS/Include/arm_math.h
 // https://www.keil.com/pack/doc/CMSIS/DSP/html/arm_linear_interp_example_f32_8c-example.html
 
 #include <arm_math.h>
@@ -9,8 +10,8 @@
 #define  NEW_FRAME            COLS*ROWS*SCALE
 
 int16_t inputVals[ROW_FRAME] = {0};                           // Array to store input values
+float32_t bilinIntOutput[ROW_FRAME] = {0};                    // Output buffer calculated from bilinear interpolation
 extern float32_t arm_bilinear_interep_table[NEW_FRAME] = {0}; // External table used for bilinear interpolation
-float32_t bilinIntOutput[NEW_FRAME] = {0};                    // Output buffer calculated from bilinear interpolation
 
 arm_bilinear_interp_instance_f32 S;
 
@@ -26,7 +27,7 @@ void loop() {
 
   for (posX = 0; posX < ROWS; posX++) {
     for (posY = 0; posY < COLS; posY++) {
-      bilinIntOutput[i] = arm_bilinear_interp_f32(&S, posX, posY );
+      bilinIntOutput[posX + posY*COLS] = arm_bilinear_interp_f32(&S, posX, posY );
     }
   }
 }
