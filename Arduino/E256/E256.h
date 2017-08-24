cbf51d9fbcef4171db18c3a351683ecd7613f462
maurinElectroTextile <maurin@datapaulette.org>
2017-08-24 15:14:46 +0200

    Added bilinear interpolation

--- Arduino/E256/E256.h
+++ Arduino/E256/E256.h
@@ -2,12 +2,55 @@
 // #define __E256_H__
 
 #include <arm_math.h>
+#include <SPI.h>
+#include <PacketSerial.h>
+
+PacketSerial serial;
+
+/*
+  PACKET SERIAL : Copyright (c) 2012-2014 Christopher Baker <http://christopherbaker.net>
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
+// The PacketSerial object.
+// It cleverly wraps one of the Serial objects.
+// While it is still possible to use the Serial object
+// directly, it is recommended that the user let the
+// PacketSerial object manage all serial communication.
+// Thus the user should not call Serial.write(), etc.
+// Additionally the user should not use the serialEvent()
+// callbacks.
+
+// Control pins to send values to the 8-BITs shift registers used on the E-256 PCB
+// shiftOut using SPI library : https://forum.arduino.cc/index.php?topic=52383.0
+// Arduino UNO - SPI PINS
+// DATA_PIN -> SPI:MOSI -> D11 // Pin connected to Data in (DS) of the firdt 74HC595 8-BIT shift register
+// CLOCK_PIN -> SPI:SCK -> D13 // Pin connected to clock pin (SH_CP) of the first 74HC595 8-BIT shift register
+// LATCH_PIN -> SPI:SS -> D10  // Pin connected to latch pin (ST_CP) of the first 74HC595 8-BIT shift register
+
 
 #define  BAUD_RATE            230400
 #define  COLS                 16
 #define  ROWS                 16
 #define  SCALE                4
-#define  FRAME                COLS*ROWS*SCALE
+#define  ROW_FRAME            COLS*ROWS
+#define  NEW_FRAME            COLS*ROWS*SCALE
 #define  CALIBRATION_CYCLES   4
 
 #define MIN_BLOB_PIX          4 // Only blobs that with more pixels than 4
@@ -15,5 +58,34 @@
 #define A0_PIN                A0  // The output of multiplexerA (SIG pin) is connected to Arduino Analog pin 0
 #define A1_PIN                A1  // The output of multiplexerB (SIG pin) is connected to Arduino Analog pin 1
 
+float32_t inputVals[ROW_FRAME] = {0};      // Array to store row input values
+int minVals[ROW_FRAME] = {0};              // Array to store smallest values
+float32_t bilinIntOutput[NEW_FRAME] = {0}; // Output buffer calculated from bilinear interpolation
+uint8_t myPacket[NEW_FRAME] = {0};              // Array to store values to transmit
+
+arm_bilinear_interp_instance_f32 S;
+
+
 void onPacket(const uint8_t* buffer, size_t size);
 void calibrate(uint8_t* id, int val, int frame);
+void bilinearInterpolation();
+
+boolean scan = true;
+boolean calibration = true;
+
+uint8_t byteC;
+uint8_t byteB;
+uint8_t byteA;
+
+// Array to store all parameters used to configure the two shift registers
+const byte setCols[COLS] = {
+  0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1,
+  0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1
+};
+
+// Array to store all parameters used to configure the two analog multiplexeurs
+const byte setRows[ROWS] = {
+  0x85, 0x87, 0x83, 0x81, 0x82, 0x84, 0x80, 0x86,
+  0x58, 0x78, 0x38, 0x18, 0x28, 0x48, 0x8, 0x68
+};
+
--- Arduino/E256/E256.ino
+++ Arduino/E256/E256.ino
@@ -1,74 +1,6 @@
 // E-256 eTextile matrix sensor shield V2.0
 
 #include "E256.h"
-#include <SPI.h>
-#include <PacketSerial.h>
-
-PacketSerial serial;
-
-/*
-  PACKET SERIAL : Copyright (c) 2012-2014 Christopher Baker <http://christopherbaker.net>
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-*/
-
-// The PacketSerial object.
-// It cleverly wraps one of the Serial objects.
-// While it is still possible to use the Serial object
-// directly, it is recommended that the user let the
-// PacketSerial object manage all serial communication.
-// Thus the user should not call Serial.write(), etc.
-// Additionally the user should not use the serialEvent()
-// callbacks.
-
-// Control pins to send values to the 8-BITs shift registers used on the E-256 PCB
-// shiftOut using SPI library : https://forum.arduino.cc/index.php?topic=52383.0
-// Arduino UNO - SPI PINS
-// DATA_PIN -> SPI:MOSI -> D11 // Pin connected to Data in (DS) of the firdt 74HC595 8-BIT shift register
-// CLOCK_PIN -> SPI:SCK -> D13 // Pin connected to clock pin (SH_CP) of the first 74HC595 8-BIT shift register
-// LATCH_PIN -> SPI:SS -> D10  // Pin connected to latch pin (ST_CP) of the first 74HC595 8-BIT shift register
-
-
-int minVals[FRAME / SCALE] = {0};           // Array to store smallest values
-int8_t bilinearInterp[FRAME * SCALE] = {0}; // Array to store Interpolated values
-uint8_t myPacket[FRAME] = {0};              // Array to store values to transmit
-
-boolean scan = true;
-boolean calibration = true;
-
-uint8_t byteC;
-uint8_t byteB;
-uint8_t byteA;
-
-// Array to store all parameters used to configure the two shift registers
-const byte setCols[COLS] = {
-  0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1,
-  0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1
-};
-
-// Array to store all parameters used to configure the two analog multiplexeurs
-const byte setRows[ROWS] = {
-  0x85, 0x87, 0x83, 0x81, 0x82, 0x84, 0x80, 0x86,
-  0x58, 0x78, 0x38, 0x18, 0x28, 0x48, 0x8, 0x68
-};
-
-arm_bilinear_interp_instance_q7 S;
-
 
 ////////////////////////////////////// SETUP
 void setup() {
@@ -81,9 +13,9 @@ void setup() {
   SPI.transfer(0xFFFFFF);   // All OFF
   digitalWrite(SS, LOW);    // set latchPin LOW
   digitalWrite(SS, HIGH);   // set latchPin HIGH
-  S.numRows = ROWS * SCALE;
-  S.numCols = COLS * SCALE;
-  S.pData = bilinearInterp;
+  S.numRows = ROWS;
+  S.numCols = COLS;
+  S.pData = &inputVals[0];
 }
 
 ////////////////////////////////////// LOOP
@@ -116,9 +48,6 @@ void loop() {
 
         int rowValue = analogRead(A0_PIN);  // Reding use to store analog inputs values
         byte sensorID = col * COLS + row; // Calculate the index of the unidimensional array
-        
-        arm_bilinear_interp_q7(&S, col, row);
-
 
         if (calibration) {
           Calibrate(sensorID, rowValue, minVals);
@@ -131,6 +60,9 @@ void loop() {
     }
     scan = false;
   }
+
+  bilinearInterpolation();
+  
   // The update() method attempts to read in
   // any incoming serial data and emits packets via
   // the user's onPacket(const uint8_t* buffer, size_t size)
@@ -138,23 +70,20 @@ void loop() {
   // The update() method should be called at the end of the loop().
   serial.update();
 
-
 }
-
 
 void Calibrate( uint8_t id, int val, int frame[] ) {
   static int calibrationCounter = 0;
 
   frame[id] += val;
   calibrationCounter++;
-  if (calibrationCounter >= CALIBRATION_CYCLES * FRAME / SCALE) {
-    for (int i = 0; i < FRAME / SCALE; i++) {
+  if (calibrationCounter >= CALIBRATION_CYCLES * ROW_FRAME) {
+    for (int i = 0; i < ROW_FRAME; i++) {
       frame[i] = frame[i] / CALIBRATION_CYCLES;
     }
     calibrationCounter = 0;
   }
   calibration = false;
-
 }
 
 // This is our packet callback.
@@ -162,7 +91,17 @@ void Calibrate( uint8_t id, int val, int frame[] ) {
 void onPacket(const uint8_t* buffer, size_t size) {
   // The send() method will encode the buffer
   // as a packet, set packet markers, etc.
-  serial.send(myPacket, FRAME / SCALE);
+  serial.send(myPacket, ROW_FRAME);
   scan = true;
 }
 
+void bilinearInterpolation() {
+  float32_t posX, posY;
+  int pos = 0;
+
+  for (posX = 0; posX < ROWS; posX += .25) {
+    for (posY = 0; posY < COLS; posY += .25) {
+      bilinIntOutput[pos++] = arm_bilinear_interp_f32(&S, posX, posY);
+    }
+  }
+}
