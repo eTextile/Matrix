bbd53beb75a3e914c49ea4e5e14f64b385be05b6
maurinElectroTextile <maurin@datapaulette.org>
2017-08-24 14:08:09 +0200

    FIX the bilinear interpolation example

--- Arduino/bilinear_interpolation/bilinear_interpolation.ino
+++ Arduino/bilinear_interpolation/bilinear_interpolation.ino
@@ -9,25 +9,29 @@
 #define  ROW_FRAME            COLS*ROWS
 #define  NEW_FRAME            COLS*ROWS*SCALE
 
-int16_t inputVals[ROW_FRAME] = {0};                           // Array to store input values
-float32_t bilinIntOutput[ROW_FRAME] = {0};                    // Output buffer calculated from bilinear interpolation
-extern float32_t arm_bilinear_interep_table[NEW_FRAME] = {0}; // External table used for bilinear interpolation
+float32_t inputVals[ROW_FRAME] = {0};      // Array to store row input values
+float32_t bilinIntOutput[NEW_FRAME] = {0}; // Output buffer calculated from bilinear interpolation
 
 arm_bilinear_interp_instance_f32 S;
 
+int pos = 0;
+
 void setup() {
-  S.numRows = ROWS * SCALE;
-  S.numCols = COLS * SCALE;
-  S.pData = &arm_bilinear_interep_table[0];
+  S.numRows = ROWS;
+  S.numCols = COLS;
+  S.pData = &inputVals[0];
 }
 
 void loop() {
 
-  uint32_t posX, posY;
+  float32_t posX, posY;
 
-  for (posX = 0; posX < ROWS; posX++) {
-    for (posY = 0; posY < COLS; posY++) {
-      bilinIntOutput[posX + posY*COLS] = arm_bilinear_interp_f32(&S, posX, posY );
+  for (posX = 0; posX < ROWS; posX + 0.25) {
+    for (posY = 0; posY < COLS; posY + 0.25) {
+      pos++;
+      bilinIntOutput[pos] = arm_bilinear_interp_f32(&S, posX, posY);
     }
   }
+  pos = 0;
 }
+
