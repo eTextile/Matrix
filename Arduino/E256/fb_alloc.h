9cee7361bcb5eae1728ce087f679a4c7f4859190
maurinElectroTextile <maurin@datapaulette.org>
2017-08-26 14:50:53 +0200

    Added fb_alloc.c & .h

---
+++ Arduino/E256/fb_alloc.h
@@ -0,0 +1,18 @@
+/*
+ * This file is part of the OpenMV project.
+ * Copyright (c) 2013-2016 Kwabena W. Agyeman <kwagyeman@openmv.io>
+ * This work is licensed under the MIT license, see the file LICENSE for details.
+ *
+ * Interface for using extra frame buffer RAM as a stack.
+ *
+ */
+#ifndef __FB_ALLOC_H__
+#define __FB_ALLOC_H__
+
+#include <stdint.h>
+
+void *fb_alloc0(uint32_t size);
+void *fb_alloc_all(uint32_t *size); // returns pointer and sets size
+void fb_free();
+
+#endif /* __FF_ALLOC_H__ */
