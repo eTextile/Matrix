adcbb743de649711e58e2e3a771dd3f047318435
maurinElectroTextile <maurin@etextile.org>
2019-06-24 11:32:07 +0200

    Updated config file

--- Firmware/MAIN/config.h
+++ Firmware/MAIN/config.h
@@ -1,50 +1,47 @@
+
 /*
   This file is part of the eTextile-matrix-sensor project - http://matrix.eTextile.org
-  Copyright (c) 2014-2018 Maurin Donneaud <maurin@etextile.org>
+  Copyright (c) 2014-2019 Maurin Donneaud <maurin@etextile.org>thun
   This work is licensed under Creative Commons Attribution-ShareAlike 4.0 International license, see the LICENSE file for details.
 */
 
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#include <SPI.h>                    // https://www.pjrc.com/teensy/td_libs_SPI.html
-/*
-  This file is part of the eTextile-matrix-sensor project - http://matrix.eTextile.org
-  Copyright (c) 2014-2019 Maurin Donneaud <maurin@etextile.org>
-  This work is licensed under Creative Commons Attribution-ShareAlike 4.0 International license, see the LICENSE file for details.
-*/
+#include <Arduino.h>
 
 /* __E256 BOARD CONTROL__*/
+
+//#define E256_X0 -1        // TODO: select X-axis origine [-1:1]
+//#define E256_Y0 -1        // TODO: select Y-axis origine [-1:1]
+
 #define BLOBS_OSC
-//#define E256_X0 -1                // TODO: select X-axis origine [-1:1]
-//#define E256_Y0 -1                // TODO: select Y-axis origine [-1:1]
 
 //#define DEBUG_ADC
 //#define DEBUG_INTERP
+//#define DEBUG_BITMAP
 //#define DEBUG_BLOBS_OSC
 //#define DEBUG_FPS
 
-//#define DEBUG_BITMAP
-//#define DEBUG_CCL
-//#define DEBUG_BLOB_ID
-//#define DEBUG_CENTER
-//#define DEBUG_LIST
-
-//#define LED_BUILTIN         13        // FIXME - Hardware SPI conflict
+//#define LED_BUILTIN         13        // FIXME - SPI (hardware) conflict
 //#define BUTTON_PIN          32        // FIXME - NO BUTTON_PIN on the E256
 
+//#define BAUD_RATE           115200
 #define BAUD_RATE             230400
-#define COLS                  16
-#define ROWS                  16
-#define DUAL_ROWS             (ROWS / 2)
+#define RAW_COLS              16
+#define RAW_ROWS              16
+#define DUAL_ROWS             (RAW_ROWS / 2)
 #define SCALE_X               4
 #define SCALE_Y               4
-#define ROW_FRAME             (COLS * ROWS)
-#define NEW_COLS              (COLS * SCALE_X)
-#define NEW_ROWS              (ROWS * SCALE_Y)
+#define RAW_FRAME             (RAW_COLS * RAW_ROWS)
+#define NEW_COLS              (RAW_COLS * SCALE_X)
+#define NEW_ROWS              (RAW_ROWS * SCALE_Y)
 #define NEW_FRAME             (NEW_COLS * NEW_ROWS)
 #define MAX_NODES             40       // Set the maximum nodes number
-#define MIN_BLOB_PIX          9       // Set the minimum blob pixels
+#define LIFO_MAX_NODES        255      // Set the maximum nodes number
+#define X_STRIDE              1
+#define Y_STRIDE              1
+#define MIN_BLOB_PIX          25       // Set the minimum blob pixels
 #define MAX_BLOB_PIX          4095     // Set the maximum blob pixels
 #define BLOB_PACKET_SIZE      7        // Blob data packet (bytes)
 
